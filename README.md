363e60c59bff0de91c664d25f4aa50ceb58cd36c
Junior <juniormbaya243@gmail.com>
2022-07-18 14:37:34 +0100

    rebuild the logic

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Fifty-Five-Test
  
- A Simple Code Snippet just for catch the title and price in DOM website.
- I used the Intersection Observer API native to Javascript, what is it ?  
+ A Simple Code Snippet to obtain the title and price information on a DOM website.
+ I used the Intersection Observer API native to Javascript, 
+ 
+ ### what is it ?  
+ An Intersection Observer is a browser API that provides a way to observe the visibility and position of a DOM element relative to the containing root element or viewport.
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// In this section, I will get from the DOM all the elements of "ProductCard"
 const myElements = document.getElementsByClassName("ProductCard");
+
+// I create an array to push the product's title and price information
 const productsList = [];
+
+// this is how I configured the intersection observer
 const config = {
   root: null,
   rootMargin: "0px",
@@ -9,7 +14,8 @@ const config = {
 const observer = new IntersectionObserver(function (events, self) {
   events.forEach((event) => {
     if (event.isIntersecting) {
-      observer.unobserve(event.target);
+      //All products caught in  the intersection are automatically added to productlist
+
       productsList.push({
         title: event.target.querySelector(".ProductCard__subtitle").children[0]
           .innerHTML,
@@ -17,10 +23,15 @@ const observer = new IntersectionObserver(function (events, self) {
           .innerHTML,
       });
 
+      //Unobserve allows me to not add all the elements already caught in the intersection above
+      observer.unobserve(event.target);
+
       console.log(productsList);
     }
   });
 }, config);
+
+// Here, I looped all my ProductCard in the DOM
 Array.prototype.forEach.call(myElements, function (article) {
   observer.observe(article);
 });
