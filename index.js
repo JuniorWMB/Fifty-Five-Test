788de683413247bfca291401622b444e1ae420a0
Junior <juniormbaya243@gmail.com>
2022-07-20 11:33:34 +0100

    add version 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//VERSION ONE
+
 // In this section, I will get from the DOM all the elements of "ProductCard"
 const myElements = document.getElementsByClassName("ProductCard");
 
@@ -34,4 +36,40 @@ const observer = new IntersectionObserver(function (events, self) {
 // Here, I looped all my ProductCard in the DOM
 Array.prototype.forEach.call(myElements, function (article) {
   observer.observe(article);
+});
+
+//VERSION TWO
+
+// In this section, I will get from the DOM all the elements of "ProductCard"
+const myElementsV2 = document.getElementsByClassName("ProductCard");
+
+// this is how I configured the intersection observer
+const configV2 = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const observerV2 = new IntersectionObserver(function (events, self) {
+  events.forEach((event) => {
+    if (event.isIntersecting) {
+      //Title
+      console.log(
+        event.target.querySelector(".ProductCard__subtitle").children[0]
+          .innerHTML
+      );
+      //Price
+      console.log(
+        event.target.querySelector(".ProductCard__price").children[0].innerHTML
+      );
+
+      //Unobserve allows me to not add all the elements already caught in the intersection above
+      observer.unobserve(event.target);
+    }
+  });
+}, configV2);
+
+// Here, I looped all my ProductCard in the DOM
+Array.prototype.forEach.call(myElementsV2, function (article) {
+  observerV2.observe(article);
 });
